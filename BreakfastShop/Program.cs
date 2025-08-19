59948bf5ca94dad84b74cc5ee7cd0333ed18c143
PatrickThompson9 <patrick@aoms-tech.com>
2025-08-19 17:25:33 +0100

    added addscope line to define when breakfastservice should be created

--- BreakfastShop/Program.cs
+++ BreakfastShop/Program.cs
@@ -1,10 +1,14 @@
+using BreakfastShop.Services.Breakfasts;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
+    builder.Services.AddScoped<IBreakfastService, BreakfastService>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.UseRouting();
     app.MapControllers();
